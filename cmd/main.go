7f7f7609d1193ae74670ecea9ce45e67dae994bd
chandresh-pancholi <chandresh@oneconcern.com>
2019-01-02 18:18:56 +0530

    fixing main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,19 @@
-package cmd
+package main
 
 import (
 	"flag"
 	"fmt"
 	"github.com/chandresh-pancholi/csi-gce/pkg/driver"
 	"github.com/golang/glog"
+	"log"
+	"os"
 )
 
 func main()  {
 	fmt.Println("hello world")
 	var (
 		endpoint = flag.String("endpoint", "unix://tmp/cmd.sock", "CSI Endpoint")
+		nodeID          = flag.String("nodeid", "", "node id")
 		//version  = flag.Bool("version", false, "Print the version and exit.")
 	)
 	flag.Parse()
@@ -28,8 +31,16 @@ func main()  {
 
 
 
-	drv := driver.NewDriver(nil, *endpoint)
+	drv := driver.NewDriver(nil, *nodeID, *endpoint)
 	if err := drv.Run(); err != nil {
 		glog.Fatalln(err)
 	}
+
+	driver, err := driver.NewGCE(nil, *nodeID, *endpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+	driver.Run()
+	os.Exit(0)
+
 }
